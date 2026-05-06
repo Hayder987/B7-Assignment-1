13d23b337255057ee1e0e587f24197b119cd3565
hayder <hayderbd4290@gmail.com>
2026-05-06 21:25:46 +0600

    solve proble 1

--- solutions.ts
+++ solutions.ts
@@ -1,1 +1,10 @@
 
+// Problem 1
+type NumberArr = number[];
+
+function filterEvenNumbers(numbers: NumberArr): NumberArr {
+  return numbers.filter(num => num % 2 === 0);
+}
+
+console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
+
